5c80060b37ac35ca0d3d1d4a55d332abff8d837c
Toby Wishart <qwenit56@gmail.com>
2019-03-05 17:13:58 +0000

    Actually fixed the old man walking

--- Assets/Scripts/Player/playerInteract.cs
+++ Assets/Scripts/Player/playerInteract.cs
@@ -81,10 +81,10 @@ public class playerInteract : MonoBehaviour {
             }
             if (item.GetComponent<objectScript>().data.ObjectName.Equals("Old Man")) {
                 npcTalk t = item.GetComponent<npcTalk>();
-                t.playDialog((soundManagerScript.oldManAndPriest)t.currentLine);
-                if (t.currentLine == t.lineCount) {
+                if (t.currentLine + 1 == t.lineCount) {
                     item.GetComponent<TestAI>().enabled = true;
                 }
+                t.playDialog((soundManagerScript.oldManAndPriest)t.currentLine);
             }
 
             if (item.GetComponent<objectScript>().data.ObjectName.Equals("Boy")) {
