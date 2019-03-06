23a62025bfde3b84d3e59c95bd4423e7b9e5f5f0
Toby Wishart <qwenit56@gmail.com>
2019-03-06 23:12:03 +0000

    Real basement (purest progression)

---
+++ Assets/Scripts/toggleTerrain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleTerrain : MonoBehaviour {
+
+    public bool enable;
+    public GameObject terrain;
+
+    private void OnTriggerEnter(Collider other) {
+        terrain.SetActive(enable);
+    }
+
+}
