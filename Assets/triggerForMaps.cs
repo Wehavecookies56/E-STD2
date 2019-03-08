4e35edc46c2f266945bf13ba383626e7790f49bc
elliott <e.desouza1996@hotmail.com>
2019-03-08 18:49:37 +0000

    switching from lower to upper in minimaps complete

---
+++ Assets/triggerForMaps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerForMaps : MonoBehaviour
+{
+
+    public GameObject MapLower;
+    public GameObject MapUpper;
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MapLower.SetActive(false);
+            MapUpper.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MapUpper.SetActive(false);
+            MapLower.SetActive(true);
+        }
+    }
+}
