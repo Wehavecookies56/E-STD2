bd0ae600bda820c59d4c051693ac58264af19d50
Unknown <d.sheshtanov@gmail.com>
2019-03-17 01:05:13 +0000

    Snake event complete
    Snake event gets triggered, snake animates, moves, rotates, chases player, damages player, runs away and disables self

---
+++ Assets/Scripts/Entities/SnakeTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTrigger : MonoBehaviour
+{
+    public Snake snake;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == snake.player)
+        {
+            snake.state = Snake.States.ENTERING;
+            GameObject.Find("soundManager").GetComponent<soundManagerScript>().playOnce(soundManagerScript.enviromentSounds.SNAKEHISS, snake.transform);
+            gameObject.SetActive(false);
+        }
+    }
+}
