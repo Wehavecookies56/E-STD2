196d0b4d5f810a7c45baa691fe8f285f335e30de
sebastian mol <sebastianmol1996@gamil.com>
2019-03-18 21:48:33 +0000

    ghosts and plate

--- Assets/onCrystalBallClick.cs
+++ Assets/onCrystalBallClick.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class onCrystalBallClick : MonoBehaviour
 {
+
+    public int counter = 0;
     public void onClick()
     {
         GameObject.FindGameObjectWithTag("description").GetComponent<descriptionManager>().activateDescription(descriptionManager.descriptionType.CRYSTALBALL);
         soundManagerScript.audioPlayer.playOnce(soundManagerScript.UIsounds.BUTTONCLICKED, gameObject.transform);
+        if(counter == 1)
+        {
+            //GameObject instance = GameObject.FindGameObjectWithTag("crystalballcam").transform.GetChild(0).GetComponent<GameObject>();
+            //instance.SetActive(true);
+            GameObject.FindGameObjectWithTag("crystalballui").GetComponent<CanvasGroup>().alpha = 1;
+            GameObject.FindGameObjectWithTag("crystalballui").GetComponent<CanvasGroup>().blocksRaycasts = true;
+          counter = 0;
+        }
+        else
+        {
+            counter++;
+        }
     }
 }
---
+++ Assets/rotateSimple.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateSimple : MonoBehaviour
+{
+    public Vector3 rot;
+
+    private void Update()
+    {
+        transform.Rotate(rot);
+    }
+}
