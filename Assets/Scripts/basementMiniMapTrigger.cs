788948a6e79205d00d47d68421a4f17e5003000d
elliott <e.desouza1996@hotmail.com>
2019-03-19 17:06:24 +0000

    made basement map work and moved crystal ball

---
+++ Assets/Scripts/basementMiniMapTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basementMiniMapTrigger : MonoBehaviour
+{
+   
+    public GameObject MapLower;
+    public GameObject MapBasement;
+    public GameObject playerIcon;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MapLower.SetActive(false);
+            playerIcon.SetActive(false);
+            MapBasement.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MapBasement.SetActive(false);
+            MapLower.SetActive(true);
+            playerIcon.SetActive(true);
+        }
+    }
+}
