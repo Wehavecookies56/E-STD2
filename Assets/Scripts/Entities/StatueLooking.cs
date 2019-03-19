4cfe241ef245e06379aa49a31e228bb270a73bf7
Unknown <d.sheshtanov@gmail.com>
2019-03-19 18:41:16 +0000

    Statue looks at player when player faces away

---
+++ Assets/Scripts/Entities/StatueLooking.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueLooking : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(transform.GetChild(0).GetComponent<MeshRenderer>().isVisible))
+            transform.LookAt(player.transform);
+    }
+}
