1a425b8c0dcaf5b5c7ea4406134a99a710c5eb03
elliott <e.desouza1996@hotmail.com>
2019-04-02 01:33:25 +0100

    added walking animation to the devil

--- Assets/DveilStopWlaking.cs
+++ Assets/DveilStopWlaking.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DveilStopWlaking : MonoBehaviour
 {
+    public GameObject Devil;
+    public GameObject DevilTrigger ;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +17,17 @@ public class DveilStopWlaking : MonoBehaviour
     {
         
     }
+   
+    
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Devil"))
         {
+            Devil.GetComponent<Animator>().SetTrigger("EnterIdle");
+            DevilTrigger.GetComponent<DevilTrigger>().KeepGoing = false;
+           
+            Destroy(DevilTrigger);
             //disable walk anim
             //play idle anim
         }
