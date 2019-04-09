67c9808258d44a96e9284207162580bfa6b9d22e
Toby <qwenit56@gmail.com>
2019-04-09 02:16:55 +0100

    Improved balcony event

---
+++ Assets/Scripts/activateBalconySpirit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateBalconySpirit : MonoBehaviour {
+    public GameObject spirit;
+
+    private void OnTriggerEnter(Collider other) {
+        spirit.SetActive(true);
+        spirit.GetComponent<Animator>().SetTrigger("run");
+    }
+}
---
+++ Assets/Scripts/balconySpirit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class balconySpirit : MonoBehaviour {
+
+    public GameObject shovel;
+
+    public void dropShovel() {
+        shovel.GetComponent<Rigidbody>().isKinematic = false;
+        shovel.transform.parent = null;
+    }
+
+    public void uppercut() {
+        transform.Rotate(new Vector3(0, 180, 0));
+        GetComponent<Rigidbody>().isKinematic = false;
+        transform.Translate(new Vector3(0, 0, -2.6f));
+    }
+
+    void Update() {
+        
+    }
+}
