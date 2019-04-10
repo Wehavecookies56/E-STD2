80099ca82bb1cccff0d7d5beb727b575bff934ef
Dan Sheshtanov <d.sheshtanov@gmail.com>
2019-04-10 16:04:47 +0100

    Added falling dust mini-event
    Also added new material, sound and prefab for the event

---
+++ Assets/Scripts/Events/FallingDustEvent.cs
@@ -0,0 +1,31 @@
+﻿//Written by Dan Sheshtanov
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDustEvent : MonoBehaviour
+{
+    //component references
+    private ParticleSystem ps;
+    private BoxCollider bc;
+    private soundManagerScript sm;
+    
+    void Start()
+    {
+        //get references
+        ps = GetComponent<ParticleSystem>();
+        bc = GetComponent<BoxCollider>();
+        sm = GameObject.Find("soundManager").GetComponent<soundManagerScript>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if collided with player, start particle system and disable collider (single play)
+        if(other.gameObject.tag == "Player")
+        {
+            ps.Play();
+            sm.playOnce(soundManagerScript.enviromentSounds.DUSTFALLING, transform);
+            bc.enabled = false;
+        }
+    }
+}
